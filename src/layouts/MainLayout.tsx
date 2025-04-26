520ca60ebebd8a5cfc56310a26236750614be966
DeveloperDolon <dolonr718@gmail.com>
2025-04-26 21:46:35 +0600

    dark mood and light moode configuration added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div>
-      This is the header
+    <div className="dark:bg-black dark:text-white bg-gray-100 text-gray-900 min-h-screen">
       <Outlet />
+      <h1 className="dark:text-red-500 text-yellow-500">Hello world from bangladesh.</h1>
     </div>
   );
 };
