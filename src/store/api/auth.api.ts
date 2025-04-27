a1ffc2144a32ee5220f537788873dfcabd27c899
DeveloperDolon <dolonr718@gmail.com>
2025-04-27 12:04:46 +0600

    user signup api implemented

---
+++ src/store/api/auth.api.ts
@@ -0,0 +1,17 @@
+import { SignupValidationType } from "../../pages/signup/signup.validation";
+import { baseApi } from "./baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    signup: builder.mutation({
+      query: (payload: SignupValidationType) => ({
+        url: "user/create",
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const { useSignupMutation } = authApi;
